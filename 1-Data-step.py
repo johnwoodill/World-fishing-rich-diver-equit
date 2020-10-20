e807fd93d140cfa66f24cdce7398946e52d360b7
johnwoodill <johnwoodill@gmail.com>
2020-10-20 11:34:53 -0700

    adding new figures

--- 1-Data-step.py
+++ 1-Data-step.py
@@ -1,39 +1,125 @@
 import pandas as pd
 import numpy as np
+import xarray
+import glob as glob
+
+
+def shan_div(indat):
+    lon_lat = indat['lon_lat'].iat[0]
+    lat = indat['lat'].iat[0]
+    lon = indat['lon'].iat[0]
+    indat = indat.assign(total = sum(indat['obs_count']))
+    indat = indat.assign(p = indat['obs_count']/indat['total'])
+    indat = indat.assign(ln_p = np.log(indat['p']))
+    indat = indat.assign(p_ln_p = indat['p'] * indat['ln_p'])
+    H = -1 * (np.sum(indat['p_ln_p']))
+    E = H / np.log(len(indat))
+    outdat = pd.DataFrame({'lon_lat': [lon_lat], 'lon': [lon], 'lat': [lat], 'H': [H], 'E': [E]})
+    return outdat
+
+
+
+# Check cmip6 raster file
+ds = xarray.open_dataset('data/chlos_Omon_GFDL-ESM4_ssp585_r1i1p1f1_gr_209501-210012.nc')
+ds_dat = ds.to_dataframe().reset_index()
+
+# Subtract 0.5 to center graid
+ds_dat = ds_dat.assign(lat = ds_dat.lat - 0.5,
+                       lon = ds_dat.lon - 0.5)
+
+# Bind GFW data
+GFW_DIR = '/data2/GFW_public/fishing_effort_10d/daily_csvs/'
+files = glob.glob('/data2/GFW_public/fishing_effort_10d/daily_csvs/*.csv')
+
+list_ = []
+for file in files:
+    df = pd.read_csv(f"{file}")
+    df = df.assign(lat = ( (df['lat_bin']/10) + 0.05),
+                   lon = ( (df['lon_bin']/10) + 0.05))
+    list_.append(df)
+
+dat = pd.concat(list_, sort=False)
+
+dat.to_csv('data/full_GFW_public_10d.csv')
 
 ### Load data
-dat = pd.read_csv('data/full_GFW_public_100d.csv', index_col=False)
+dat = pd.read_csv('data/full_GFW_public_10d.csv', index_col=False)
 
+### Column names
 # Index(['date', 'lat_bin', 'lon_bin', 'flag', 'geartype',
 #        'vessel_hours', 'fishing_hours', 'mmsi_present', 'lat', 'lon',
 #        'lon_lat'],
 #       dtype='object')
 
 ### Aggregate to 10th degree
-dat = dat.assign(lon = round(dat['lon'], 1),
-                 lat = round(dat['lat'], 1))
+dat = dat.assign(lon = round(dat['lon'], 0),
+                 lat = round(dat['lat'], 0),
+                 year = pd.DatetimeIndex(dat['date']).year)
+
+### Set lon/lat -0 to 0 for aggregation
+dat = dat.assign(lon = np.where(dat['lon'] == -0.0, 0.0, dat['lon']))
+dat = dat.assign(lat = np.where(dat['lat'] == -0.0, 0.0, dat['lat']))
 
 ### Get unique lon/lat grids
 dat = dat.assign(lon_lat = dat['lon'].astype(str) + "_" + dat['lat'].astype(str))
 
+
 ### Aggregate to 10th degree lon/lat: 
 
+# Total Fishing Effort
+feffort_dat = dat.groupby(['year', 'lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'sum'}).reset_index()
+feffort_dat = dat.groupby(['lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'mean'}).reset_index()
+feffort_dat.to_csv('data/total_fishing_effort.csv', index=False)
+
+# Total Fishing Effort by nation
+feffort_nat_dat = dat.groupby(['year', 'lon_lat', 'flag']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'sum'}).reset_index()
+feffort_nat_dat = dat.groupby(['lon_lat', 'flag']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'mean'}).reset_index()
+feffort_nat_dat.to_csv('data/total_fishing_effort_nation.csv', index=False)
+
+
 # Species Richness
-flag_dat = dat.groupby(['date', 'lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'flag': 'nunique'}).reset_index()
-flag_dat.to_csv('data/daily_species_richness.csv', index=False)
+richness_dat = dat.groupby(['lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'flag': 'nunique'}).reset_index()
+richness_dat.to_csv('data/total_species_richness.csv', index=False)
 
 
-# vessel_hours
-vhours_dat = dat.groupby(['date', 'lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'vessel_hours': 'sum'}).reset_index()
+# Shannon Diversity Index
+sdiv_dat = dat.groupby(['lon_lat', 'flag']).agg({'lat': 'mean', 'lon': 'mean', 'year': 'count'}).reset_index()
+sdiv_dat = sdiv_dat.rename(columns={'year': 'obs_count'})
+
+shan_divi = sdiv_dat.groupby('lon_lat').apply(lambda x: shan_div(x))
+shan_divi.reset_index(drop=True)
+shan_divi.to_csv('data/shannon_div_equ.csv', index=False)
 
 
 
 
-# fishing_hours
-fhours_dat = dat.groupby(['date', 'lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'sum'}).reset_index()
+def flag_int(indat, flag_i, flag_j):
+    indat = indat[( indat['flag'] == flag_i) | (indat['flag'] == flag_j)]
+    return len(indat)
+    
+
+# Flag interactions
+fint_dat = dat.groupby(['lon_lat', 'flag']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'sum'}).reset_index()
 
 
 
-# mmsi_present
-mmsi_dat = dat.groupby(['date', 'lon_lat']).agg({'lat': 'mean', 'lon': 'mean', 'mmsi': 'nunique'}).reset_index()
+unique_flags = fint_dat.flag.unique()
 
+retdat = pd.DataFrame()
+for i in unique_flags:
+    for j in unique_flags:
+        flag_1 = i
+        flag_2 = j
+        Ai = len(fint_dat[fint_dat['flag'] == flag_1])
+        Aj = len(fint_dat[fint_dat['flag'] == flag_2])
+        red_dat = fint_dat[(fint_dat['flag'] == flag_1) | (fint_dat['flag'] == flag_2)]
+        ret_flag_int = red_dat.groupby('lon_lat').apply(lambda x: flag_int(x, flag_1, flag_2))
+        Aij = Ai / sum(ret_flag_int)
+        outdat = pd.DataFrame({'flag_1': [flag_1], 'flag_2': [flag_2], 'interaction': [Aij]})
+        retdat = pd.concat([retdat, outdat])
+        print(f"{flag_1}-{flag_2}")
+        
+        
+        
+        
+retdat.to_csv('data/flag_interactions.csv', index=False)
--- Figures.R
+++ Figures.R
@@ -1,16 +1,309 @@
 library(tidyverse)
 library(marmap)
+library(ggmap)
+library(viridis)
+library(mapdata)
 
 setwd("~/Projects/World-fishing-rich-diver-equit/")
 
-mdat <- read_csv("data/daily_species_richness.csv")
+fdat <- as.data.frame(read_csv("data/total_fishing_effort.csv"))
 
-mdat$month <- month(mdat$date)
-mdat$day <- day(mdat$date)
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
-pdat <- filter(mdat, month == 2 & day == 1)
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
-ggplot(mdat, aes(lon, lat, fill=flag)) + 
-  stat_density2d()
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+pdat <- filter(pdat, fishing_hours > 0)
+
+# Setup map
+# mp1 <- fortify(map(fill=TRUE, plot=FALSE))
+# mp2 <- mp1
+# mp2$long <- mp2$long + 360
+# mp2$group <- mp2$group + max(mp2$group) + 1
+# mp <- rbind(mp1, mp2)
+
+ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.5, inherit.aes = FALSE) +
+  # First segment to the east
+  annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+  annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+  annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+  annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+  annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+  annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+  annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+  geom_polygon()  + 
+  scale_color_viridis(direction = -1) +
+  theme_bw() +
+  labs(x="Longitude", y="Latitude", 
+       title="Average Annual Fishing Effort in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(.97, 0.27)) +
+  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  guides(fill = FALSE,
+       color = guide_colorbar(title.position = "top",
+                              frame.colour = "black",
+                              barwidth = .5,
+                              barheight = 10,
+                              label.position = 'left')) +
+  NULL
+
+ggsave("figures/total_fishing_effort.png", width=10, height=6)
+#
 
 
+
+
+
+
+
+
+### Fishing Effort by Flag
+
+# Chinese
+setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+fdat <- as.data.frame(read_csv("data/total_fishing_effort_nation.csv"))
+
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
+
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
+
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+pdat <- filter(pdat, fishing_hours > 0)
+
+pdat <- filter(pdat, flag == "CHN")
+
+ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.5, inherit.aes = FALSE) +
+  # First segment to the east
+  annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+  annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+  annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+  annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+  annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+  annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+  annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+  geom_polygon()  + 
+  scale_color_viridis(direction = -1, limits = c(0.01, 12)) +
+  theme_bw() +
+  labs(x="Longitude", y="Latitude", 
+       title="Chinese Average Annual Fishing Effort in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(.97, 0.27)) +
+  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  guides(fill = FALSE,
+       color = guide_colorbar(title.position = "top",
+                              frame.colour = "black",
+                              barwidth = .5,
+                              barheight = 10,
+                              label.position = 'left')) +
+  NULL
+
+ggsave("figures/total_fishing_effort_CHN.png", width=10, height=6)
+
+
+
+
+### Fishing Effort by Flag
+
+# USA
+setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+fdat <- as.data.frame(read_csv("data/total_fishing_effort_nation.csv"))
+
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
+
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
+
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+pdat <- filter(pdat, fishing_hours > 0)
+
+pdat <- filter(pdat, flag == "USA")
+
+ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.5, inherit.aes = FALSE) +
+  # First segment to the east
+  annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+  annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+  annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+  annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+  annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+  annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+  annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+  geom_polygon()  + 
+  scale_color_viridis(direction = -1, limits = c(0.01, 12)) +
+  theme_bw() +
+  labs(x="Longitude", y="Latitude", 
+       title="USA Average Annual Fishing Effort in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(.97, 0.27)) +
+  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  guides(fill = FALSE,
+       color = guide_colorbar(title.position = "top",
+                              frame.colour = "black",
+                              barwidth = .5,
+                              barheight = 10,
+                              label.position = 'left')) +
+  NULL
+
+ggsave("figures/total_fishing_effort_USA.png", width=10, height=6)
+
+
+
+
+### Species Richness
+
+
+setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+fdat <- as.data.frame(read_csv("data/total_species_richness.csv"))
+
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
+
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
+
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+
+ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_point(data = pdat, aes(lon, lat, color=flag), size = 0.5, inherit.aes = FALSE) +
+  # First segment to the east
+  annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+  annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+  annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+  annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+  annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+  annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+  annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+  geom_polygon()  + 
+  scale_color_viridis_c(direction = -1, limits = c(1, 30)) +
+  theme_bw() +
+  labs(x="Longitude", y="Latitude", 
+       title="Species Richness in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(.97, 0.27)) +
+  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  guides(fill = FALSE,
+       color = guide_colorbar(title.position = "top",
+                              frame.colour = "black",
+                              barwidth = .5,
+                              barheight = 10,
+                              label.position = 'left')) +
+  NULL
+
+ggsave("figures/species_richness.png", width=10, height=6)
+
+
+
+
+
+
+### Shannon Diversity
+
+
+setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+fdat <- as.data.frame(read_csv("data/shannon_div_equ.csv"))
+
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
+
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
+
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+
+ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_point(data = pdat, aes(lon, lat, color=H), size = 0.5, inherit.aes = FALSE) +
+  # First segment to the east
+  annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+  annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+  annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+  annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+  annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+  annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+  annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+  geom_polygon()  + 
+  scale_color_viridis_c(direction = -1, limits = c(0, 3)) +
+  theme_bw() +
+  labs(x="Longitude", y="Latitude", 
+       title="Shannon Diversity Index in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(.97, 0.27)) +
+  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  guides(fill = FALSE,
+       color = guide_colorbar(title.position = "top",
+                              frame.colour = "black",
+                              barwidth = .5,
+                              barheight = 10,
+                              label.position = 'left')) +
+  NULL
+
+ggsave("figures/shannon_div_index.png", width=10, height=6)
+
+
+
+
+
+### Shannon Equity
+
+
+setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+fdat <- as.data.frame(read_csv("data/shannon_div_equ.csv"))
+
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
+
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
+
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+
+ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_point(data = pdat, aes(lon, lat, color=E), size = 0.5, inherit.aes = FALSE) +
+  # First segment to the east
+  annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+  annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+  annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+  annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+  annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+  annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+  annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+  geom_polygon()  + 
+  scale_color_viridis_c(direction = -1, limits = c(0, 1)) +
+  theme_bw() +
+  labs(x="Longitude", y="Latitude", 
+       title="Shannon Equitability Index in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(.97, 0.27)) +
+  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  guides(fill = FALSE,
+       color = guide_colorbar(title.position = "top",
+                              frame.colour = "black",
+                              barwidth = .5,
+                              barheight = 10,
+                              label.position = 'left')) +
+  NULL
+
+ggsave("figures/shannon_equ_index.png", width=10, height=6)
