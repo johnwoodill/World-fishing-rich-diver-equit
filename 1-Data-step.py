bd7bd7cf7831d996f4ca9de3d641613971189b08
johnwoodill <johnwoodill@gmail.com>
2020-10-27 09:15:34 -0700

    added new figs

--- 1-Data-step.py
+++ 1-Data-step.py
@@ -40,10 +40,11 @@ for file in files:
 
 dat = pd.concat(list_, sort=False)
 
-dat.to_csv('data/full_GFW_public_10d.csv')
+# Load vessel characteristics
+vessel_dat = pd.read_csv('/data2/GFW_public/fishing_vessels/fishing_vessels.csv')
 
-### Load data
-dat = pd.read_csv('data/full_GFW_public_10d.csv', index_col=False)
+# Merge on mmsi
+dat = dat.merge(vessel_dat, how='left', on='mmsi')
 
 ### Column names
 # Index(['date', 'lat_bin', 'lon_bin', 'flag', 'geartype',
@@ -63,6 +64,23 @@ dat = dat.assign(lat = np.where(dat['lat'] == -0.0, 0.0, dat['lat']))
 ### Get unique lon/lat grids
 dat = dat.assign(lon_lat = dat['lon'].astype(str) + "_" + dat['lat'].astype(str))
 
+dat = dat.sort_values('date').reset_index(drop=True)
+
+dat = dat.dropna()
+
+# Columns
+# ------------------------------------------------------------------------------
+# Index(['date', 'lat_bin', 'lon_bin', 'mmsi', 'fishing_hours', 'lat', 'lon',
+#        'flag', 'geartype', 'length', 'tonnage', 'engine_power', 'active_2012',
+#        'active_2013', 'active_2014', 'active_2015', 'active_2016', 'year',
+#        'lon_lat'],
+#       dtype='object')
+# ------------------------------------------------------------------------------
+
+dat.to_csv('data/full_GFW_public_10d.csv')
+
+### Load data
+dat = pd.read_csv('data/full_GFW_public_10d.csv', index_col=False)
 
 ### Aggregate to 10th degree lon/lat: 
 
@@ -87,39 +105,59 @@ sdiv_dat = dat.groupby(['lon_lat', 'flag']).agg({'lat': 'mean', 'lon': 'mean', '
 sdiv_dat = sdiv_dat.rename(columns={'year': 'obs_count'})
 
 shan_divi = sdiv_dat.groupby('lon_lat').apply(lambda x: shan_div(x))
-shan_divi.reset_index(drop=True)
+shan_divi = shan_divi.reset_index(drop=True)
 shan_divi.to_csv('data/shannon_div_equ.csv', index=False)
 
 
+def flag_count(indat, flat_i, flag_j):
+    indat = indat[( indat['flag'] == flag_i ) | ( indat['flag'] == flag_j )]
+    if len(indat) == 2:
+        return 1
+    else:
+        return 0
 
 
-def flag_int(indat, flag_i, flag_j):
-    indat = indat[( indat['flag'] == flag_i) | (indat['flag'] == flag_j)]
-    return len(indat)
+def flag_int(fint_dat, flag_i, flag_j):
+    if flag_i != flag_j:
+        Ai = len(fint_dat[fint_dat['flag'] == flag_i])
+        red_dat = fint_dat[(fint_dat['flag'] == flag_i) | (fint_dat['flag'] == flag_j)]
+        red_dat = red_dat[['lon_lat', 'flag', 'fishing_hours']].pivot(index='lon_lat', columns='flag', values = 'fishing_hours').reset_index()
+        red_dat = red_dat.dropna()
+        ret_flag_int = red_dat[(red_dat.iloc[:, 1] > 0) & (red_dat.iloc[:, 2] > 0)]
+        
+        #ret_flag_int = red_dat.groupby('lon_lat').apply(lambda x: flag_count(x, flag_i, flag_j))
+        Aij = len(ret_flag_int) / Ai
+        return (flag_i, flag_j, Aij)
+    else:
+        return (flag_i, flag_j, 1)
     
-
+        
+    
 # Flag interactions
 fint_dat = dat.groupby(['lon_lat', 'flag']).agg({'lat': 'mean', 'lon': 'mean', 'fishing_hours': 'sum'}).reset_index()
 
+# Flag interactions
+unique_flags = sorted(fint_dat.flag.unique())
+
+unique_flags = ['USA', 'CHN']
+
+res = [flag_int(fint_dat, i, j) for i in unique_flags for j in unique_flags]
+
+flag_1 = [x[0] for x in res]
+flag_2 = [x[1] for x in res]
+interaction = [x[2] for x in res]
+save_dat = pd.DataFrame({'flag_1': flag_1, 'flag_2': flag_2, 'interaction': interaction})
+save_dat.to_csv('data/flag_interactions.csv', index=False)
 
 
-unique_flags = fint_dat.flag.unique()
+retdat = pd.read_csv('data/flag_interactions.csv')
 
-retdat = pd.DataFrame()
-for i in unique_flags:
-    for j in unique_flags:
-        flag_1 = i
-        flag_2 = j
-        Ai = len(fint_dat[fint_dat['flag'] == flag_1])
-        Aj = len(fint_dat[fint_dat['flag'] == flag_2])
-        red_dat = fint_dat[(fint_dat['flag'] == flag_1) | (fint_dat['flag'] == flag_2)]
-        ret_flag_int = red_dat.groupby('lon_lat').apply(lambda x: flag_int(x, flag_1, flag_2))
-        Aij = Ai / sum(ret_flag_int)
-        outdat = pd.DataFrame({'flag_1': [flag_1], 'flag_2': [flag_2], 'interaction': [Aij]})
-        retdat = pd.concat([retdat, outdat])
-        print(f"{flag_1}-{flag_2}")
-        
-        
-        
-        
-retdat.to_csv('data/flag_interactions.csv', index=False)
+# Spread data frame to matrix
+mat_retdat = retdat.pivot(index='flag_1', columns='flag_2', values='interaction')
+mat_retdat.to_csv('data/flag_interactions_matrix.csv')
+
+mat_retdat = np.matrix(mat_retdat)
+np.save('data/flag_interactions_matrix.npy', mat_retdat) 
+
+
+
--- Figures.R
+++ Figures.R
@@ -3,8 +3,19 @@ library(marmap)
 library(ggmap)
 library(viridis)
 library(mapdata)
+library(sf)
+library(raster)
+
 
 setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+eezs <- read_sf('data/World_EEZ_v11_20191118_HR_0_360/eez_boundaries_v11_0_360.shp')
+
+mpas <- read_sf('data/WDPA_WDOECM_marine_shp-points.shp')
+mpas <- st_shift_longitude(mpas)
+
+mpas <- read_sf('data/mpa_shapefiles/vlmpa.shp')
+mpas <- st_shift_longitude(mpas)
 
 fdat <- as.data.frame(read_csv("data/total_fishing_effort.csv"))
 
@@ -12,7 +23,7 @@ fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
 # Subset within WCP
 fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
-fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
 fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
 # Bind and filter out fishing_hours > 0
@@ -20,15 +31,18 @@ pdat <- rbind(fdat1, fdat2, fdat3)
 pdat <- filter(pdat, fishing_hours > 0)
 
 # Setup map
-# mp1 <- fortify(map(fill=TRUE, plot=FALSE))
-# mp2 <- mp1
-# mp2$long <- mp2$long + 360
-# mp2$group <- mp2$group + max(mp2$group) + 1
-# mp <- rbind(mp1, mp2)
+mp1 <- fortify(map(fill=TRUE, plot=FALSE))
+mp2 <- mp1
+mp2$long <- mp2$long + 360
+mp2$group <- mp2$group + max(mp2$group) + 1
+mp <- rbind(mp1, mp2)
 
 ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
-  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.5, inherit.aes = FALSE) +
-  # First segment to the east
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
+  
+    # First segment to the east
   annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
   annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
   annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
@@ -36,23 +50,23 @@ ggplot(data = mp, aes(x = long, y = lat, group = group)) +
   annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
   annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
   annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
-  geom_polygon()  + 
+  geom_polygon()  +
   scale_color_viridis(direction = -1) +
   theme_bw() +
   labs(x="Longitude", y="Latitude", 
        title="Average Annual Fishing Effort in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position = c(.97, 0.27)) +
-  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+        legend.position = c(.94, 0.27)) +
+  coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
   guides(fill = FALSE,
        color = guide_colorbar(title.position = "top",
                               frame.colour = "black",
-                              barwidth = .5,
-                              barheight = 10,
+                              barwidth = .25,
+                              barheight = 6,
                               label.position = 'left')) +
   NULL
 
-ggsave("figures/total_fishing_effort.png", width=10, height=6)
+ggsave("figures/total_fishing_effort.png", width=8, height=4)
 #
 
 
@@ -73,7 +87,7 @@ fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
 # Subset within WCP
 fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
-fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
 fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
 # Bind and filter out fishing_hours > 0
@@ -83,7 +97,9 @@ pdat <- filter(pdat, fishing_hours > 0)
 pdat <- filter(pdat, flag == "CHN")
 
 ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
-  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.5, inherit.aes = FALSE) +
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
   # First segment to the east
   annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
   annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
@@ -98,17 +114,18 @@ ggplot(data = mp, aes(x = long, y = lat, group = group)) +
   labs(x="Longitude", y="Latitude", 
        title="Chinese Average Annual Fishing Effort in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position = c(.97, 0.27)) +
-  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+        legend.position = c(.94, 0.27)) +
+  # coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
   guides(fill = FALSE,
        color = guide_colorbar(title.position = "top",
                               frame.colour = "black",
-                              barwidth = .5,
-                              barheight = 10,
+                              barwidth = .25,
+                              barheight = 6,
                               label.position = 'left')) +
   NULL
 
-ggsave("figures/total_fishing_effort_CHN.png", width=10, height=6)
+ggsave("figures/total_fishing_effort_CHN.png", width=8, height=4)
 
 
 
@@ -124,7 +141,7 @@ fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
 # Subset within WCP
 fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
-fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
 fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
 # Bind and filter out fishing_hours > 0
@@ -134,7 +151,9 @@ pdat <- filter(pdat, fishing_hours > 0)
 pdat <- filter(pdat, flag == "USA")
 
 ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
-  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.5, inherit.aes = FALSE) +
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
   # First segment to the east
   annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
   annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
@@ -149,20 +168,107 @@ ggplot(data = mp, aes(x = long, y = lat, group = group)) +
   labs(x="Longitude", y="Latitude", 
        title="USA Average Annual Fishing Effort in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position = c(.97, 0.27)) +
-  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+        legend.position = c(.94, 0.27)) +
+  # coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
   guides(fill = FALSE,
        color = guide_colorbar(title.position = "top",
                               frame.colour = "black",
-                              barwidth = .5,
-                              barheight = 10,
+                              barwidth = .25,
+                              barheight = 6,
                               label.position = 'left')) +
   NULL
 
-ggsave("figures/total_fishing_effort_USA.png", width=10, height=6)
+ggsave("figures/total_fishing_effort_USA.png", width=8, height=4)
 
 
 
+
+### Fishing Effort top 5 flags
+
+# Chinese
+setwd("~/Projects/World-fishing-rich-diver-equit/")
+
+fdat <- as.data.frame(read_csv("data/total_fishing_effort_nation.csv"))
+
+fdat %>% group_by(flag) %>% summarise(sum_fishing_hours = sum(fishing_hours)) %>% arrange(-sum_fishing_hours) %>% head(6)
+
+
+
+#   flag  sum_fishing_hours
+#   <chr>             <dbl>
+# 1 TWN              32963.
+# 2 CHN              30935.
+# 3 JPN              20514.
+# 4 KOR              16572.
+# 5 ESP              15380.
+# 6 USA              10619.
+
+top_5 <- c("TWN", "CHN", "JPN", "KOR", "ESP")
+
+fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
+
+# Subset within WCP
+fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
+fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
+
+# Bind and filter out fishing_hours > 0
+pdat <- rbind(fdat1, fdat2, fdat3)
+pdat <- filter(pdat, fishing_hours > 0)
+
+i = 3
+
+for (i in 1:length(top_5)){
+  flag_ = top_5[i]
+  mdat <- filter(pdat, flag == flag_ | flag == "USA") 
+  mdat <- mdat %>% group_by(lon_lat) %>% mutate(nn = n())
+  mdat$flag_overlap <- ifelse(mdat$nn == 2, paste0(flag_, "-USA"), mdat$flag)
+  mdat$flag_overlap <- factor(mdat$flag_overlap, levels = c("USA", flag_, paste0(flag_, "-USA")))
+  mdat <- as.data.frame(mdat)
+  
+  
+p1 <- ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = mdat, aes(lon, lat, color=flag_overlap), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
+    # First segment to the east
+    annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
+    annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
+    annotate("segment", x = -130 + 360, xend = -130 + 360, y = 0, yend = -60) +
+    annotate("segment", x = -130 + 360, xend = -210 + 360, y = -60, yend = -60) +
+    annotate("segment", x = -210 + 360, xend = -210 + 360, y = -60, yend = -55) +
+    annotate("segment", x = -210 + 360, xend = -220 + 360, y = -55, yend = -55) +
+    annotate("segment", x = -220 + 360, xend = -220 + 360, y = -55, yend = -38) +
+    geom_polygon()  + 
+    scale_color_viridis_d() +
+    theme_bw() +
+    labs(x="Longitude", y="Latitude", 
+         title=paste0("USA-", flag_, " Interactions"), color=NULL) +
+    theme(plot.title = element_text(hjust = 0.5),
+          legend.position = c(.917, 0.092)) +
+    coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
+    guides(color = guide_legend(override.aes = list(size = 3))) +
+    NULL
+  
+  ggsave(plot=p1, paste0('figures/', "USA-", flag_, "_interactions.png"), width = 8, height = 6)
+  saveRDS(p1, file = paste0("data/", flag_, "-USA.rds"))
+  print(flag_)
+
+}
+
+
+p1 <- readRDS("data/TWN-USA.rds")
+p2 <- readRDS("data/CHN-USA.rds")
+p3 <- readRDS("data/JPN-USA.rds")
+p4 <- readRDS("data/KOR-USA.rds")
+p5 <- readRDS("data/ESP-USA.rds")
+
+plot_grid(p1, p2, p3, p4, p5, ncol=2)
+
+# plot_grid(p1, p1, p1, p1, p1, ncol=2)
+
+ggsave("figures/all_interactions.png", width = 16, height = 6*3)
 
 ### Species Richness
 
@@ -175,14 +281,16 @@ fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
 # Subset within WCP
 fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
-fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
 fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
 # Bind and filter out fishing_hours > 0
 pdat <- rbind(fdat1, fdat2, fdat3)
 
 ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
-  geom_point(data = pdat, aes(lon, lat, color=flag), size = 0.5, inherit.aes = FALSE) +
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
   # First segment to the east
   annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
   annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
@@ -197,17 +305,18 @@ ggplot(data = mp, aes(x = long, y = lat, group = group)) +
   labs(x="Longitude", y="Latitude", 
        title="Species Richness in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position = c(.97, 0.27)) +
-  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+        legend.position = c(.94, 0.27)) +
+  # coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
   guides(fill = FALSE,
        color = guide_colorbar(title.position = "top",
                               frame.colour = "black",
-                              barwidth = .5,
-                              barheight = 10,
+                              barwidth = .25,
+                              barheight = 6,
                               label.position = 'left')) +
   NULL
 
-ggsave("figures/species_richness.png", width=10, height=6)
+ggsave("figures/species_richness.png", width=8, height=4)
 
 
 
@@ -225,14 +334,16 @@ fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
 # Subset within WCP
 fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
-fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
 fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
 # Bind and filter out fishing_hours > 0
 pdat <- rbind(fdat1, fdat2, fdat3)
 
 ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
-  geom_point(data = pdat, aes(lon, lat, color=H), size = 0.5, inherit.aes = FALSE) +
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
   # First segment to the east
   annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
   annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
@@ -247,17 +358,17 @@ ggplot(data = mp, aes(x = long, y = lat, group = group)) +
   labs(x="Longitude", y="Latitude", 
        title="Shannon Diversity Index in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position = c(.97, 0.27)) +
-  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+        legend.position = c(.94, 0.27)) +
+  coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
   guides(fill = FALSE,
        color = guide_colorbar(title.position = "top",
                               frame.colour = "black",
-                              barwidth = .5,
-                              barheight = 10,
+                              barwidth = .25,
+                              barheight = 6,
                               label.position = 'left')) +
   NULL
 
-ggsave("figures/shannon_div_index.png", width=10, height=6)
+ggsave("figures/shannon_div_index.png", width=8, height=4)
 
 
 
@@ -274,14 +385,16 @@ fdat$lon <- ifelse(fdat$lon < 0, fdat$lon + 360, fdat$lon)
 
 # Subset within WCP
 fdat1 <- filter(fdat, lon <= -150 + 360 & lon >= 100 & lat >= 0)
-fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat <= 0 & lat >= -55)
+fdat2 <- filter(fdat, lon <= -130 + 360 & lon >= 140 & lat < 0 & lat >= -55)
 fdat3 <- filter(fdat, lon <= -130 + 360 & lon >= 150 & lat <= -55 & lat >= -60)
 
 # Bind and filter out fishing_hours > 0
 pdat <- rbind(fdat1, fdat2, fdat3)
 
 ggplot(data = mp, aes(x = long, y = lat, group = group)) + 
-  geom_point(data = pdat, aes(lon, lat, color=E), size = 0.5, inherit.aes = FALSE) +
+  geom_sf(data = mpas, color = 'grey', alpha = 0.5, fill = 'grey', size = 0.1, inherit.aes = FALSE) +
+  geom_point(data = pdat, aes(lon, lat, color=fishing_hours), size = 0.25, inherit.aes = FALSE, shape=15) +
+  geom_sf(data = eezs, color = '#374a6d', alpha = 0.5, fill = NA, size = 0.1, inherit.aes = FALSE) +
   # First segment to the east
   annotate("segment", x = -150 + 360, xend = -150 + 360, y = 0, yend = 60) +
   annotate("segment", x = -150 + 360, xend = -130 + 360, y = 0, yend = 0) +
@@ -296,14 +409,26 @@ ggplot(data = mp, aes(x = long, y = lat, group = group)) +
   labs(x="Longitude", y="Latitude", 
        title="Shannon Equitability Index in the Western-Central Pacific Ocean \n 2012-2016", color=NULL) +
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position = c(.97, 0.27)) +
-  coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+        legend.position = c(.94, 0.27)) +
+  # coord_cartesian(xlim = c(100, 250), ylim = c(-60, 60)) +
+  coord_sf(xlim = c(100, 250), ylim = c(-60, 60)) +
   guides(fill = FALSE,
        color = guide_colorbar(title.position = "top",
                               frame.colour = "black",
-                              barwidth = .5,
-                              barheight = 10,
+                              barwidth = .25,
+                              barheight = 6,
                               label.position = 'left')) +
   NULL
 
-ggsave("figures/shannon_equ_index.png", width=10, height=6)
+ggsave("figures/shannon_equ_index.png", width=8, height=4)
+
+
+
+
+### Heat map of interactions
+
+idat <- as.data.frame(read_csv("data/flag_interactions.csv"))
+
+idat <- filter(idat, interaction != 0 & interaction != 1) %>% arrange(-interaction)
+
+ggplot(idat, aes(flag_1, flag_2, fill=interaction)) + geom_tile()
